eada77a8763c25378476e8c34ad9944c43be934d
Atakan Yıldız <atakanyildiz27@gmail.com>
2022-05-18 14:19:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello World!");
+int sayi= 21;
+int sayi2= 23;
+int sayi3=91;
+int sayi4=10;
+string s="sonuc";
+//Sayısal İşlemler
+float toplama= sayi3+sayi;
+Console.WriteLine(s+toplama);
+
+float cikarma= sayi2-sayi4;
+Console.WriteLine(s+cikarma);
+
+float carpma= sayi2*sayi;
+Console.WriteLine(s+carpma);
+
+float bolme= sayi3/sayi4;
+Console.WriteLine(s+toplama);
